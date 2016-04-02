15b29189edd637a053cdc88b419af786fc8c5e79
nstockwell6806 <noah.stockwell@edgewoodhs.org>
2016-04-02 18:43:19 -0500

    Added Features
    Overloaded the LED change function for LED string class.

--- main/header.h
+++ main/header.h
@@ -1,45 +1,61 @@
 #define totalChannelNum 1
-int pins [totalChannelNum][3];
+int pins[totalChannelNum][3];
 class LedString
 {
-  public:
-    int channel, rVal, gVal, bVal;
-    LedString(int channelTemp)                                      //constructor; channelTemp is the input channel
-    {
-      channel = channelTemp;                                        //permanently store channel
-      analogWrite(pins[channel][0], rVal = 15);
-      analogWrite(pins[channel][1], gVal = 15);
-      analogWrite(pins[channel][2], bVal = 15);
-    }
-    void changeVal(int val, char pin)                               //change value of one pin
-    {
-      switch (pin)
-      {
-        case 'r' :
-          analogWrite(pins[channel][0], rVal = val);
-          break;
-        case 'g' :
-          analogWrite(pins[channel][1], gVal = val);
-          break;
-        case 'b' :
-          analogWrite(pins[channel][2], bVal = val);
-          break;
-        default :                                                   //catchall to turn LEDS to standby if not r/g/b
-          int i;
-          for (i = 0; i < 3; i++) {
-            digitalWrite(pins[channel][i], 15);
-          }
-      }
-    }
+
+public:
+	int channel, rVal, gVal, bVal;
+	LedString(int channelTemp)                                      //constructor; channelTemp is the input channel
+	{
+		channel = channelTemp;                                        //permanently store channel
+		analogWrite(pins[channel][0], rVal = 15);
+		analogWrite(pins[channel][1], gVal = 15);
+		analogWrite(pins[channel][2], bVal = 15);
+	}
+	void changeVal(int val, char pin)                                                    //change value of one pin
+	{
+		switch (pin)
+		{
+		case 'r':
+			analogWrite(pins[channel][0], rVal = val);
+			break;
+		case 'g':
+			analogWrite(pins[channel][1], gVal = val);
+			break;
+		case 'b':
+			analogWrite(pins[channel][2], bVal = val);
+			break;
+		}
+	}
+	void changeVal(int val1, char pin1, int val2, char pin2)                             //change value of two pins
+	{
+		changeVal(val1, pin1);
+		changeVal(val2, pin2);
+	}
+	void changeVal(int val1, char pin1, int val2, char pin2, int val3, char pin3)        //change value of three pins
+	{
+		changeVal(val1, pin1);
+		changeVal(val2, pin2);
+		changeVal(val3, pin3);
+	}
+	void fade(int val, char pin)
+	{
+		switch pin
+			case 'r':
+
+	}
+
+private:
+	void 
 };
 
-void setPins(int channelIn, int pin1, int pin2, int pin3 )                    //set pins on board to a certain channel in the array pins[][]
+void setPins(int channelIn, int pin1, int pin2, int pin3)                    //set pins on board to a certain channel in the array pins[][]
 {
-  int pinsIn[3] = {pin1, pin2, pin3};
-  int i;
-  for(i = 0; i < 3; i++)
-  {
-    pins[channelIn][i] = pinsIn[i];
-  }
+	int pinsIn[3] = { pin1, pin2, pin3 };
+	int i;
+	for (i = 0; i < 3; i++)
+	{
+		pins[channelIn][i] = pinsIn[i];
+	}
 }
 
