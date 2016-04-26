e314a071e023396afb163c0959b36310b43cedb4
nstockwell6806 <noah.stockwell@edgewoodhs.org>
2016-04-26 11:31:27 -0500

    Updated main.ino
    Switched constructor to setup instead of looping

--- main/main.ino
+++ main/main.ino
@@ -2,9 +2,10 @@
 
 void setup() {
 Serial.begin(9600);
+setPins( 0, 7, 8, 9 );          //initialize pins 7, 8, and 9 to channel 0
+LedString String1 (0);          //setup string 1 running on channel 0
 }
 
 void loop() {
-setPins( 0, 7, 8, 9 );          //initialize pins 7, 8, and 9 to channel 0
-LedString String1 (0);          //setup string 1 running on channel 0
+String1.fade();
 }
